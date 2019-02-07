8bb1b95b1095921ed095c89b02366ed12d64d378
Kev McCarthy <kev@totallyratted.com>
2019-02-07 14:48:00 +0000

    Fix documentation error

--- RESTore/RESToreException.cs
+++ RESTore/RESToreException.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Liberator.RESTore
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class RESToreException : Exception
     {
+        /// <summary>
+        /// An exception thrown by the library
+        /// </summary>
+        /// <param name="message">The message to send to the exception</param>
         public RESToreException(string message): base(message)
         {
 
