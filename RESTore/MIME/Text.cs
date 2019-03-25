3bf24aa11afd62c3064c600160551aedb1d8c925
Kev McCarthy <kev@totallyratted.com>
2019-03-25 09:47:16 +0000

    MIME types added as static strings

---
+++ RESTore/MIME/Text.cs
@@ -0,0 +1,49 @@
+﻿namespace Liberator.RESTore.MIME
+{
+    public static partial class Text
+    {
+        public static string OneD_InterleavedParity = "text/1d-interleaved-parityfec ";
+        public static string Cache_Manifest = "text/cache-manifest ";
+        public static string Calendar = "text/calendar ";
+        public static string CSS = "text/css";
+        public static string CSV = "text/csv ";
+        public static string CSV_Schema = "text/csv-schema ";
+        public static string Directory = "text/directory ";
+        public static string DNS = "text/dns ";
+        public static string EcmaScript = "text/ecmascript";
+        public static string EncapRtp = "text/encaprtp ";
+        public static string Enriched = "text/enriched";
+        public static string Example = "text/example ";
+        public static string FwdRed = "text/fwdred ";
+        public static string GrammarRefList = "text/grammar-ref-list";
+        public static string HTML = "text/html";
+        public static string JavaScript = "text/javascript";
+        public static string JcrCnd = "text/jcr-cnd ";
+        public static string Markdown = "text/markdown ";
+        public static string Mizar = "text/mizar ";
+        public static string N3 = "text/n3 ";
+        public static string Parameters = "text/parameters ";
+        public static string ParityFec = "text/parityfec";
+        public static string Plain = "text/plain";
+        public static string ProvenanceNotation = "text/provenance-notation ";
+        public static string RaptorFec = "text/raptorfec ";
+        public static string RED = "text/RED ";
+        public static string RFC822_Headers = "text/rfc822-headers ";
+        public static string RichText = "text/richtext";
+        public static string RTF = "text/rtf";
+        public static string AESCM = "text/rtp-enc-aescm128 ";
+        public static string RtpLoopBack = "text/rtploopback ";
+        public static string RTX = "text/rtx ";
+        public static string SGML = "text/sgml ";
+        public static string Strings = "text/strings ";
+        public static string T140 = "text/t140 ";
+        public static string TabSeparatedValues = "text/tab-separated-values ";
+        public static string TrOff = "text/troff";
+        public static string Turtle = "text/turtle";
+        public static string UlpFec = "text/ulpfec";
+        public static string UriList = "text/uri-list";
+        public static string vCard = "text/vcard";
+        public static string XML = "text/xml";
+        public static string XML_ExternalParsedEntity = "text/xml-external-parsed-entity";
+    }
+}
---
+++ RESTore/MIME/TextVND.cs
@@ -0,0 +1,37 @@
+﻿namespace Liberator.RESTore.MIME
+{
+    public static partial class Text
+    {
+        public static string VND_a = "text/vnd.a";
+        public static string VND_abc = "text/vnd.abc ";
+        public static string VND_ASCII_Art = "text/vnd.ascii-art ";
+        public static string VND_Curl = "text/vnd.curl ";
+        public static string VND_DebianCopyright = "text/vnd.debian.copyright ";
+        public static string VND_DM_ClientScript = "text/vnd.DMClientScript";
+        public static string VND_DVD_SubTitle = "text/vnd.dvb.subtitle";
+        public static string VND_ThemeDescriptor = "text/vnd.esmertec.theme-descriptor";
+        public static string VND_Fly = "text/vnd.fly";
+        public static string VND_FMI_Flexstor = "text/vnd.fmi.flexstor";
+        public static string VND_GML = "text/vnd.gml";
+        public static string VND_GraphViz = "text/vnd.graphviz";
+        public static string VND_HGL = "text/vnd.hgl";
+        public static string VND_In3D_3DML = "text/vnd.in3d.3dml";
+        public static string VND_In3D_Spot = "text/vnd.in3d.spot";
+        public static string VND_IPTC_NewsML = "text/vnd.IPTC.NewsML";
+        public static string VND_IPTC_NITF = "text/vnd.IPTC.NITF";
+        public static string VND_LatexZ = "text/vnd.latex-z";
+        public static string VND_MotorolaReflex = "text/vnd.motorola.reflex";
+        public static string VND_MS_MediaPackage = "text/vnd.ms-mediapackage";
+        public static string VND_N2P_CommCentreCommand = "text/vnd.net2phone.commcenter.command";
+        public static string VND_MSML_BasicLayout = "text/vnd.radisys.msml-basic-layout";
+        public static string VND_WarpScript = "text/vnd.senx.warpscript";
+        public static string VND_UriCatalogue = "text/vnd.si.uricatalogue";
+        public static string VND_AppDescriptor = "text/vnd.sun.j2me.app-descriptor";
+        public static string VND_Linguist = "text/vnd.trolltech.linguist";
+        public static string VND_WAP_SI = "text/vnd.wap.si";
+        public static string VND_WAP_SL = "text/vnd.wap.sl";
+        public static string VND_WAP_WML = "text/vnd.wap.wml";
+        public static string VND_WAP_WML_Script = "text/vnd.wap.wmlscript";
+
+    }
+}
