4049e813e20b53ae1e7554cb8298dd39d3115738
Kev McCarthy <kev@totallyratted.com>
2019-03-25 09:47:35 +0000

    Remove excess usings

--- RESTore/MIME/Application.cs
+++ RESTore/MIME/Application.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Liberator.RESTore.MIME
+﻿namespace Liberator.RESTore.MIME
 {
     public static class Application
     {
